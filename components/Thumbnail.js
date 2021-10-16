6b38295eeeb0ec0008a953f95c52a792905427d4
KMF21 <ikoojojoseph817@gmail.com>
2021-10-16 15:05:30 +0100

    thumbnail correction2.1

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -10,15 +10,17 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
     >
       <Image
+      className="p-2"
         layout="responsive"
         height={1080}
         width={1920}
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
           `${BASE_URL}${result.poster_path}`
+        
         }
       />
-      <div className="p-4">
+      <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
           {result.title || result.original_name}
